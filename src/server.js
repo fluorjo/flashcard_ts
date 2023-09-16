4f5144f7882e194684c156dd15aedd6c84dc9109
fluorjo <fluorjo@naver.com>
2023-09-17 08:10:03 +0900

    Requests

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
+app.get("/",()=>console.log("dddddddd"));
+
 const handleListening=()=>console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT,handleListening);
