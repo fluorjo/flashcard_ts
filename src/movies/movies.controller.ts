0e6c22a950abfe82828ea39b34722d955714fa08
fluorjo <fluorjo@naver.com>
2023-09-19 14:10:58 +0900

    lecture 2.1 More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Delete,Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller,Delete,Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,13 +7,19 @@ export class MoviesController {
         return "This will return all movies";
     }
 
+    @Get("search")
+    search(@Query('year') searchingYear:string){
+        return `We are searching for a movie made after: ${searchingYear} `;
+    }
+
     @Get("/:id")
     getOne(@Param("id") id: string){
         return `This will return one movie with the id: ${id}`;
     }
     
     @Post()
-    create(){
+    create(@Body() movieData){
+        console.log(movieData);
         return "This will create a movie";
     }
 
@@ -23,7 +29,12 @@ export class MoviesController {
     }
     
     @Patch("/:id")
-    patch(@Param('id') movieId:string){
-        return `This will patch a movie with the id: ${movieId}`;
+    patch(@Param('id') movieId:string, @Body() updateData){
+        return {
+            updatedMovie:movieId,
+            ...updateData,
+        };
     }
+
+
 }
