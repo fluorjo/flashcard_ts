f43b50bd9a58167835e229d7d4ae79468a1b223d
fluorjo <fluorjo@naver.com>
2023-09-22 08:20:27 +0900

    format on save

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,19 +1,25 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { MoviesService } from './movies.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { MoviesService } from './movies.service'
 
 describe('MoviesService', () => {
-  let service: MoviesService;
+  let service: MoviesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
-    }).compile();
+    }).compile()
 
-    service = module.get<MoviesService>(MoviesService);
-  });
+    service = module.get<MoviesService>(MoviesService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    expect(service).toBeDefined()
+  })
 
-});
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll()
+      expect(result).toBeInstanceOf(Array)
+    })
+  })
+})
