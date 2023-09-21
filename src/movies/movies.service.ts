3303b5fe72d35fa76714ad85fddc949f3dd2ce2a
fluorjo <fluorjo@naver.com>
2023-09-21 13:11:19 +0900

    2.3 Movies Service part Two  - update data

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -21,7 +21,7 @@ export class MoviesService {
 
     deleteOne(id:string ){
         this.getOne(id)
-        this.movies.filter(movie=>movie.id !==+id);
+        this.movies= this.movies.filter(movie=>movie.id !==+id);
         //return true; 
     }
 
@@ -30,5 +30,11 @@ export class MoviesService {
             id:this.movies.length+1,
             ...movieData,
         });
+    }
+
+    update(id:string, updateData){
+        const movie = this.getOne(id);
+        this.deleteOne(id);
+        this.movies.push({...movie, ...updateData});
     }
 }
