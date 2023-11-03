b54f2c57c8972f778253bbb59c40b1a44ca46862
fluorjo <fluorjo@naver.com>
2023-11-03 09:56:00 +0900

    reset

---
+++ project/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi:false,
+};
