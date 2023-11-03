aba359e827c6fbd88d16d7920569221a0e73c631
fluorjo <fluorjo@naver.com>
2023-11-03 20:59:23 +0900

    초기 구성 세팅, objecttype 약간

---
+++ project/server/src/entities/card.ts
@@ -0,0 +1,13 @@
+import { Field, Int, ObjectType } from 'type-graphql'
+
+@ObjectType()
+export class card {
+  @Field(() => Int, { description: '카드 아이디' })
+  id: number
+
+  @Field(() => Int, { description: '제작자 아이디' })
+  director_id: number
+
+  @Field(() => Int, { description: '작성일' })
+  created_date: number
+}
