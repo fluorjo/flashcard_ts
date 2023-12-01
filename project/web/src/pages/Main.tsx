59def7f031dc65bff82ab225b86a3e0de89df83b
fluorjo <fluorjo@naver.com>
2023-12-02 05:06:08 +0900

    commonLayout, Navbar

--- project/web/src/pages/Main.tsx
+++ project/web/src/pages/Main.tsx
@@ -1,11 +1,5 @@
 import CommonLayout from '../components/CommonLayout'
-import FilmList from '../components/film/FilmList'
 import { Heading } from '@chakra-ui/react'
 export default function Main(): React.ReactElement {
-  return (
-    <CommonLayout>
-      <Heading>와!!!</Heading>
-      <FilmList />
-    </CommonLayout>
-  )
+  return <CommonLayout>main</CommonLayout>
 }
