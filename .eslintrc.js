56d2b723e1c2bb17de1bd14261ca0875da97f60a
fluorjo <fluorjo@naver.com>
2023-12-06 19:07:24 +0900

    all reset. install nest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,79 +1,25 @@
 module.exports = {
-  root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'airbnb',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  plugins: ['react', 'react-hooks'],
+  root: true,
   env: {
-    browser: true,
-    es6: true,
     node: true,
+    jest: true,
   },
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
-
-    semi: ['error', 'never'],
-    'max-len': [
-      'error',
-      {
-        code: 80,
-        tabWidth: 2,
-        ignoreComments: true,
-        ignoreTrailingComments: true,
-        ignoreStrings: true,
-        ignoreUrls: true,
-        ignoreTemplateLiterals: true,
-        ignorePattern: '^import\\s.+\\sfrom\\s.+;$',
-      },
-    ],
-    'no-use-before-define': 'off',
-    'linebreak-style': 'off',
-    camelcase: 'warn',
-    'max-classes-per-file': 'off',
-    'class-methods-use-this': 'off',
-    'import/prefer-default-export': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        ts: 'never',
-        jsx: 'never',
-        tsx: 'never',
-      },
-    ],
-    'no-underscore-dangle': 0,
-    // react
-    'react/jsx-filename-extension': [
-      2,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
-    'react/require-default-props': 0,
-    'react/jsx-props-no-spreading': 1,
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-    // typescript
-    '@typescript-eslint/no-use-before-define': 2,
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
-  ignorePatterns: ['generated/**/*.tsx'],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.ts', '.jsx', '.tsx'],
-      },
-    },
-  },
-}
+};
--- project/web/src/pages/Main.tsx
+++
@@ -1,5 +0,0 @@
-import CommonLayout from '../components/commonLayout'
-import { Heading } from '@chakra-ui/react'
-export default function Main(): React.ReactElement {
-  return <CommonLayout>main</CommonLayout>
-}
