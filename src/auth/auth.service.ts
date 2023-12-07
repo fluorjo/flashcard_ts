42d529eba88450c21faf64608338f49cf62e218b
fluorjo <fluorjo@naver.com>
2023-12-08 05:08:00 +0900

    feat: install bcrypt, add auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { CreateUserDto } from 'src/user/user.dto'
+import { UserService } from 'src/user/user.service'
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async register(userDto: CreateUserDto) {
+    const user = await this.userService.getUser(userDto.email)
+    if (user) {
+      throw new HttpException(
+        'A user with that username already exists.',
+        HttpStatus.BAD_REQUEST,
+      )
+    }
+
+    const encryptedPassword = bcrypt.hashSync(userDto.password, 10)
+
+    try {
+      const user = await this.userService.createUser({
+        ...userDto,
+        password: encryptedPassword,
+      })
+      user.password = undefined
+      return user
+    } catch (error) {
+      throw new HttpException('server error', 500)
+    }
+  }
+}
