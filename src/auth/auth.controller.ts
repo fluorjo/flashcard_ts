0d36cd46c84e691e9627d5cb196c5810209b61d8
fluorjo <fluorjo@naver.com>
2023-12-11 08:02:37 +0900

    feat: add validateUser method to auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post, Request, Response } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { CreateUserDto } from 'src/user/user.dto'
 
@@ -9,5 +9,21 @@ export class AuthController {
   @Post('/register')
   async register(@Body() userDto: CreateUserDto) {
     return await this.authService.register(userDto)
+  }
+
+  @Post('login')
+  async login(@Request() req, @Response() res) {
+    const userInfo = await this.authService.validateUser(
+      req.body.email,
+      req.body.password,
+    )
+
+    if (userInfo) {
+      res.cookie('login', JSON.stringify(userInfo), {
+        httpOnly: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      })
+    }
+    return res.send({ message: 'login success' })
   }
 }
