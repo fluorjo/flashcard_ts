a7ee7a5d4c04d2972296b1323ff331e7fe1e3cc2
fluorjo <fluorjo@naver.com>
2023-12-15 02:51:03 +0900

    왜 로그인2에 쿠키 생성이 안 되지

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,29 +1,63 @@
-import { Body, Controller, Post, Request, Response } from '@nestjs/common'
-import { AuthService } from './auth.service'
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  Response,
+  UseGuards,
+} from '@nestjs/common'
+import { LoginGuard } from './auth.guard'
 import { CreateUserDto } from 'src/user/user.dto'
+import { AuthService } from './auth.service'
 
-@Controller('auth')
+@Controller('auth') // ❶ 컨트롤러 생성
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {} // ❷ AuthService를 주입받음
 
-  @Post('/register')
+  @Post('register') // ❸ register 주소로 POST로 온 요청 처리
+  // ❹ class-validator가 자동으로 유효성 검증
   async register(@Body() userDto: CreateUserDto) {
-    return await this.authService.register(userDto)
+    return await this.authService.register(userDto) // ❺ authService를 사용해 user 정보 저장
   }
-
   @Post('login')
   async login(@Request() req, @Response() res) {
+    // ❶ Request, Response를 둘 다 사용
+    // ❷ validateUser를 호출해 유저 정보 획득
     const userInfo = await this.authService.validateUser(
       req.body.email,
       req.body.password,
     )
 
+    // ❸ 유저 정보가 있으면, 쿠키 정보를 Response에 저장
     if (userInfo) {
       res.cookie('login', JSON.stringify(userInfo), {
-        httpOnly: false,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: false, // ❹ 브라우저에서 읽을 수 있도록 함
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7day 단위는 밀리초
       })
     }
     return res.send({ message: 'login success' })
+  }
+
+  @UseGuards(LoginGuard) //  ❶ LoginGuard 사용
+  @Post('login2')
+  async login2(@Request() req, @Response() res) {
+    // ❷ 쿠키 정보는 없지만 request에 user 정보가 있다면 응답값에 쿠키 정보 추가
+    if (!req.cookies['login'] && req.user) {
+      // 응답에 쿠키 정보 추가
+      res.cookie('login', JSON.stringify(req.user), {
+        httpOnly: true,
+        // maxAge: 1000 * 60 * 60 * 24 * 7, // 1day
+        maxAge: 1000 * 10, // ❸ 로그인 테스트를 고려해 10초로 설정
+      })
+    }
+    return res.send({ message: 'login2 success' })
+  }
+
+  // ❹ 로그인을 한 때만 실행되는 메서드
+  @UseGuards(LoginGuard)
+  @Get('test-guard')
+  testGuard() {
+    return '로그인된 때에만 이 글이 보입니다.'
   }
 }
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,29 +1,37 @@
 import { CanActivate, Injectable } from '@nestjs/common'
 import { AuthService } from './auth.service'
 
-@Injectable()
+@Injectable() // ❶ Injectable이 있으니 프로바이더
 export class LoginGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  // ❷ CanActivate 인터페이스 구현
+  constructor(private authService: AuthService) {} // ❸ authService를 주입받음
+
   async canActivate(context: any): Promise<boolean> {
+    // ❹ CanActivate 인터페이스의 메서드
+    // ❺ 컨텍스트에서 리퀘스트 정보를 가져옴
     const request = context.switchToHttp().getRequest()
 
+    // ❻ 쿠키가 있으면 인증된 것
     if (request.cookies['login']) {
       return true
     }
 
+    // ❼ 쿠키가 없으면 request의 body 정보 확인
     if (!request.body.email || !request.body.password) {
       return false
     }
 
+    // ❽ 인증 로직은 기존의 authService.validateUser를 사용한다.
     const user = await this.authService.validateUser(
       request.body.email,
       request.body.password,
     )
 
+    // 유저 정보가 없으면 false를 반환
     if (!user) {
       return false
     }
-
+    // ❿ 있으면 request에 user 정보를 추가하고 true를 반환
     request.user = user
     return true
   }
