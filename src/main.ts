ce530e8716ffa4c3f1766bf1580617d667662405
fluorjo <fluorjo@naver.com>
2023-12-16 01:09:55 +0900

    feat: install passport, express-session

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session'
+import * as passport from 'passport'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser())
+  app.use(
+    session({
+      secret: 'very-important-secret',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 },
+    }),
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
   await app.listen(3000)
 }
 bootstrap()
