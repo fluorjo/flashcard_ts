d49da735b71a1a3e4db6fddef7c56225a753cb10
fluorjo <fluorjo@naver.com>
2023-12-17 11:11:33 +0900

    eslint no unused vars

--- src/auth/session.serializer.ts
+++ src/auth/session.serializer.ts
@@ -21,6 +21,7 @@ export class SessionSerializer extends PassportSerializer {
       done(new Error('No User'), null)
       return
     }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...userInfo } = user
     done(null, userInfo)
   }
