df7674bebb84a00972370ba50a632c41b4e3be4c
fluorjo <fluorjo@naver.com>
2023-12-21 04:24:58 +0900

    feat: edit user entity

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -9,7 +9,7 @@ export class User {
   @Column({ unique: true })
   email: string
 
-  @Column()
+  @Column({ nullable: true })
   password: string
 
   @Column()
@@ -17,4 +17,7 @@ export class User {
 
   @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
   createdDt: Date = new Date()
+
+  @Column({ nullable: true })
+  providerId: string
 }
