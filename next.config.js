2315e1a96681137e83e2a98f2541b82f35d7457a
fluorjo <fluorjo@naver.com>
2023-12-23 02:59:17 +0900

    edit next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...compilerConfig,
+        reactRemoveProperties: { properties: ["^data-testid$"] },
+      };
+    }
+    return compilerConfig;
+  })(),
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+  //       destination: `${process.env.API_BASE_URL}/:match*`,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
